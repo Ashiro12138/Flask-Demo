4c1305bb857450d8b4021a99b4a1862ade3af762
Jason Wang <24618@bbc.qld.edu.au>
2017-02-09 21:55:19 +1000

    C:/Users/24618/AppData/Local/Programs/Git/upload has been tested under the same internet from another device, the result was successful. also added a log folder, currently manually saving the logs, in the future thinking of a way to make every run of the server save logs by it self

--- hello.py
+++ hello.py
@@ -1,13 +1,21 @@
-from flask import Flask,render_template,abort,request,redirect,url_for
+from flask import Flask,render_template,abort,request,redirect,url_for,send_from_directory
 from werkzeug.utils import secure_filename
+from werkzeug import SharedDataMiddleware
 import os
 
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = r'C:\Users\24618\Documents\AAA-Term 1\AAA-IPT\Python\PY35\Flask-Demo\uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 #http://flask.pocoo.org/docs/0.12/patterns/fileuploads/#uploading-files
+
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
 
 banList = [] #Insert IPs
 
@@ -68,14 +76,55 @@ def show_blog(page=1):
 def chat():
     return "Chat in develop"
 
-@app.route('/upload',methods=['GET','POST'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['the_file']
-        f.save('/uploads'+secure_filename(f.filename))
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+from werkzeug import SharedDataMiddleware
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+
+# @app.route('/upload',methods=['GET','POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         f = request.files['the_file']
+#         f.save('/uploads'+secure_filename(f.filename))
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(debug=True,host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True,host='0.0.0.0', port=port)
+    # app.run(debug=True)
