680bb52e5ba93959e9a8a40b47053367912127e6
Jason Wang <24618@bbc.qld.edu.au>
2017-02-13 16:14:15 +1000

    Ex01 and Ex02 done

--- Ex02/Ex02.py
+++ Ex02/Ex02.py
@@ -5,7 +5,23 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     method = request.method
-    return render_template("forms.html",method=method)
+    if method == "GET":
+        return render_template("forms.html")
+    text1 = request.form["text1"]
+    password1 = request.form["password1"]
+    red = request.form.get("red")
+    blue = request.form.get("blue")
+    green = request.form.get("green")
+    dic = {"green":green, "blue":blue, "red":red}
+    choice = request.form["size"]
+
+    return render_template("forms.html",
+                            method=method,
+                            text1=text1,
+                            password1=password1,
+                            dic=dic,
+                            choice=choice,
+                            )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
